85b77da5ccd080d327ec970c348749e043c63301
Paul Smith <p@ulsmith.net>
2017-01-19 21:30:11 +0000

    Removed notice log to console.

--- src/core.js
+++ src/core.js
@@ -48,7 +48,7 @@ export default class Core {
 		// register custom element
 		var protoWrap = {prototype: proto};
 		if (!!ext) protoWrap.extends = ext;
-		try { document.registerElement(name, protoWrap); } catch(e) { console.log('Notice: skipping "' + name + '", already registered as a custom component.'); }
+		try { document.registerElement(name, protoWrap); } catch(e) { }
 	}
 
 	static getThis()
